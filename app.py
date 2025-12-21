272a63fa766654b4fbd7815eb95178ea8080c63a
songsong <song020817@gmail.com>
2025-12-21 13:28:40 +0900

    app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.add_middleware(
         # "http://localhost:3000",
         # "http://127.0.0.1:3000",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
